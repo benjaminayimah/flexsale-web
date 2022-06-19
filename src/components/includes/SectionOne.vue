4ed7ea8ee9d5eeb4686eac1a3c9f504b1829ad59
Benjamin <benjaminayimah@gmail.com>
2022-06-19 13:30:32 +0000

    section 1

---
+++ src/components/includes/SectionOne.vue
@@ -0,0 +1,65 @@
+<template>
+    <section id="features-overview" class="fx jc-c">
+        <div class="section-wrapper fx">
+            <div class="fx-col jc-c features-col-1">
+                <!-- <div> -->
+                <h1>Packed with cool features</h1>
+                <div class="description">Flexsale is comes with useful features with new features rolling out every month.</div>
+                <!-- </div> -->
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="120" viewBox="0 0 286.94 124.044">
+                        <path  d="M12198.741-2677.957a3.711,3.711,0,0,1-3.7-3.7v-33.3a3.711,3.711,0,0,1,3.7-3.7l18.5.018a3.7,3.7,0,0,1,3.7,3.681v33.3a3.711,3.711,0,0,1-3.7,3.7Zm0-7.4h18.5v-25.9h-18.5Zm-64.555,7.4a4.112,4.112,0,0,1-4.122-4.125v-68.72a4.114,4.114,0,0,1,4.122-4.125h46.732a4.116,4.116,0,0,1,4.125,4.125v68.72a4.114,4.114,0,0,1-4.125,4.125Zm20.963-5.515a2.437,2.437,0,0,0,2.4,2.423,2.466,2.466,0,0,0,2.423-2.423,2.438,2.438,0,0,0-2.423-2.4A2.422,2.422,0,0,0,12155.149-2683.472Zm-19.586-5.481h43.981v-57.728h-43.981Zm-201.563,11v-15.5h31.011a15.549,15.549,0,0,1-15.5-15.507v-77.526a15.549,15.549,0,0,1,15.5-15.507h124.044a15.551,15.551,0,0,1,15.5,15.507v77.526a15.551,15.551,0,0,1-15.5,15.507h31.011v15.5Zm31.011-31.011h124.044v-77.526h-124.044Z" transform="translate(-11934 2802.001)" fill="#fff"/>
+                    </svg>
+                </div>
+            </div>
+            <div class="features">
+                <div class="grid-item"></div>
+                <div class="grid-item"></div>
+                <div class="grid-item"></div>
+                <div class="grid-item"></div>
+            </div>
+        </div>
+    </section>
+</template>
+<script>
+export default {
+    name: 'SectionOne',
+}
+</script>
+<style lang="scss" scoped>
+#features-overview{
+    background-color: $bg-color;
+}
+.section-wrapper{
+    width: 80%;
+    padding: 80px 0;
+    gap: 30px;
+    .features-col-1{
+        border-radius: 24px;
+        height: 620px;
+        width: 40%;
+        background-color: #3a53e3;
+        padding: 20px 50px;
+        color: #fff;
+        h1 {
+            font-size: 3.6rem;
+            font-weight: 600;
+        }
+        .description{
+            width: 75%;
+            padding: 20px 0;
+        }
+    }
+    .features {
+        width: 60%;
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        gap: 20px;
+        .grid-item{
+            background-color: #fff;
+            border-radius: 18px;
+
+        }
+    }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,60 +1,21 @@
 <template>
-  <section id="banner" class="fx jc-c ai-c" :style="{ 'minHeight': getWindowHeight-250 + 'px'}">
-    <div class="banner-wrapper fx">
-      <div class="fx-col banner-caption-wrapper gap-32">
-        <h1><span>A Smart</span> way to manage your inventory.</h1>
-        <div class="caption-sub">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt.</div>
-        <div class="fx button-wrapper"><router-link to="" class="a-button button-primary">Create Free Account</router-link></div>
-      </div>
-      <div class="banner-image fx ai-c">
-        <img src="../../public/img/home-page.svg" alt="">
-      </div>
-    </div>
-  </section>
+  <banner-section v-bind:winHeight="getWindowHeight" />
+  <section-one />
 </template>
 
 <script>
 // @ is an alias to /src
 import { mapGetters } from 'vuex'
+import BannerSection from '../components/includes/BannerSection.vue'
+import SectionOne from '../components/includes/SectionOne.vue'
 import scrollTopMixin from '../mixins/scrollTopMixin'
 export default {
+  components: { BannerSection, SectionOne },
   name: 'Home',
   computed: mapGetters(['getWindowHeight']),
   mixins: [scrollTopMixin]
 }
 </script>
 <style lang="scss" scoped>
-.banner-wrapper{
-  width: 80%;
-}
-.banner-caption-wrapper{
-  width: 55%;
-  h1 {
-    font-size: 6rem;
-    line-height: 1;
-    span {
-      color: $primary;
-    }
-  }
-  .caption-sub{
-    width: 70%;
-    font-size: 1.3rem;
-    color: $gray;
-  }
-  a {
-    padding: 25px 28px;
-    font-size: 1.1rem;
-  }
-}
-.banner-image{
-  width: 30%;
-  flex-grow: 1;
-  // background-image: url('../../public/img/banner-colors.png');
-  // background-size: cover;
-  // background-repeat: no-repeat;
-  // background-position: 50% 50%;
-  img {
-    width: 100%;
-  }
-}
+
 </style>
