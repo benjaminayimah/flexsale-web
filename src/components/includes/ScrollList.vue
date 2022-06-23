7900516771c04b5da432703041b449e155252461
Benjamin <benjaminayimah@gmail.com>
2022-06-23 01:08:55 +0000

    sections update

---
+++ src/components/includes/ScrollList.vue
@@ -0,0 +1,94 @@
+<template>
+    <li class="fx-col fx-js" :class="{ 'show-overlay' : showOverlay}" :style="{'backgroundImage': commingsoon.color}">
+        <div class="card-overlay"></div>
+        <div class="content-holder">
+            <div class="coming-soon"><span>Coming soon</span></div>
+            <h2>Invoicing</h2>
+            <div class="button" @click="toggleOverlay">
+                <svg xmlns="http://www.w3.org/2000/svg" height="17" viewBox="0 0 22.001 21.867">
+                    <path d="M10805.638-10751.066h2v9.933h9.637v2h-9.637v9.935h-2v-9.935h-10.364v-2h10.364Z" transform="translate(-10795.273 10751.066)" fill="#0e142c"/>
+                </svg>
+            </div>
+        </div>
+    </li>
+</template>
+<script>
+export default {
+    name: 'SectionScrollList',
+    props: ['commingsoon'],
+    data () {
+        return {
+            showOverlay: false
+        }
+    },
+    methods: {
+        toggleOverlay() {
+            this.showOverlay = !this.showOverlay
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+li {
+    color: #fff;
+    height: 520px;
+    width: 460px;
+    background-color: #fff;
+    border-radius: 24px;
+    position: relative;
+}
+.coming-soon{
+    display: flex;
+    margin-bottom: 15px;
+    span {
+        display: flex;
+        font-size: 13px;
+        padding: 6px 10px;
+        border: 1px solid #fff;
+        border-radius: 20px;
+    }
+}
+.button{
+    height: 38px;
+    width: 38px;
+    background-color: #fff;
+    border-radius: 50%;
+    display: grid;
+    place-items: center;
+    cursor: pointer;
+    transition: 0.3s all;
+    position: absolute;
+    bottom: 20px;
+    right: 20px;
+    z-index: 2;
+    &:hover {
+        background-color: $gray-light;
+    }
+    &:active {
+        transform: scale(0.9);
+    }
+}
+.content-holder{
+    padding: 30px;
+}
+.card-overlay{
+    position: absolute;
+    top: 0;
+    width: inherit;
+    height: inherit;
+    background-color: $dark;
+    border-radius: inherit;
+    transition: 0.5s all;
+    opacity: 0;
+    visibility: hidden;
+}
+.show-overlay {
+    .card-overlay{
+        opacity: 1;
+        visibility: visible;
+    }
+    .button{
+        transform: rotate(45deg);
+    }
+}
+</style>
--- src/components/includes/SectionScroll.vue
+++ src/components/includes/SectionScroll.vue
@@ -5,19 +5,7 @@
             <div class="list-hold">
                 <div class="list-content">
                     <ul>
-                        <li class="fx-col fx-js" v-for="supplier in suppliersALT" :key="supplier.id" :style="{'backgroundImage': supplier.color}">
-                            <div>
-                                <div class="coming-soon"><span>Coming soon</span></div>
-                                <h2>Invoicing</h2>
-                            </div>
-                            <div class="fx fx-end">
-                                <div class="button">
-                                    <svg xmlns="http://www.w3.org/2000/svg"  height="20" viewBox="0 0 22.001 21.867">
-                                        <path d="M10805.638-10751.066h2v9.933h9.637v2h-9.637v9.935h-2v-9.935h-10.364v-2h10.364Z" transform="translate(-10795.273 10751.066)" fill="#0e142c"/>
-                                    </svg>
-                                </div>
-                            </div>
-                        </li>
+                        <scroll-list v-for="commingsoon in CommingSoons" :key="commingsoon.id" v-bind:commingsoon="commingsoon" />
                     </ul>
                 </div>
             </div>
@@ -26,11 +14,13 @@
 </section>
 </template>
 <script>
+import ScrollList from './ScrollList.vue'
 export default {
+  components: { ScrollList },
     name: 'SectionScroll',
     data () {
         return {
-            suppliersALT: [
+            CommingSoons: [
                 {id: 1, index: 0, color: 'linear-gradient(to right bottom, #d02fa5, #d637ac, #dc3fb4, #e247bb, #e84ec3, #eb54c9, #ed59cf, #f05fd5, #ef64db, #ef69e1, #ee6ee7, #ed73ed)'},
                 {id: 2, index: 1, color: 'linear-gradient(to right bottom, #2f60d0, #3767d6, #3f6edc, #4775e2, #4e7ce8, #5281ea, #5686ec, #5a8bee, #5d8eec, #6092ea, #6395e8, #6798e6)'},
                 {id: 3, index: 3, color: 'linear-gradient(to right bottom, #e8ad2b, #e9b135, #ebb53e, #ecb846, #eebc4e, #eebe53, #edc157, #edc35c, #ebc55f, #e9c661, #e8c864, #e6c967)'},
@@ -83,43 +73,7 @@ export default {
             flex-direction: row;
             margin: 0;
             gap: 32px;
-            li {
-                color: #fff;
-                height: 450px;
-                width: 450px;
-                background-color: #fff;
-                border-radius: 24px;
-                padding: 30px;
-                // display: grid;
-                // place-items: center;
-            }
-            .coming-soon{
-                display: flex;
-                margin-bottom: 15px;
-                span {
-                    display: flex;
-                    font-size: 13px;
-                    padding: 6px 10px;
-                    border: 1px solid #fff;
-                    border-radius: 20px;
-                }
-            }
-            .button{
-                height: 40px;
-                width: 40px;
-                background-color: #fff;
-                border-radius: 50%;
-                display: grid;
-                place-items: center;
-                cursor: pointer;
-                transition: 0.2s all;
-                &:hover {
-                    background-color: $gray-light;
-                }
-                &:active {
-                    transform: scale(0.9);
-                }
-            }
+            
         }
     }
 }
