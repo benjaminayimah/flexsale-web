d09dd9002687ab1eff21e7e4965192e41e205d0b
Benjamin <benjaminayimah@gmail.com>
2022-06-29 18:57:34 +0000

    release

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,30 +2,30 @@
   <banner-section v-bind:winHeight="getWindowHeight" v-bind:appHostname="getAppHostname"/>
   <section-row v-bind:payload="rowWithImage" />
   <section-features-overview />
-  <section-two />
+  <!-- <section-two /> -->
   <!-- <section-one /> -->
-  <section-three />
-  <section-four />
-  <section-scroll />
-  <section-sub-hero />
-  <section-row v-bind:payload="rowNoImage" />
+  <!-- <section-three /> -->
+  <!-- <section-four /> -->
+  <!-- <section-scroll /> -->
+  <!-- <section-sub-hero /> -->
+  <!-- <section-row v-bind:payload="rowNoImage" /> -->
 </template>
 
 <script>
 // @ is an alias to /src
 import { mapGetters } from 'vuex'
 import BannerSection from '../components/includes/BannerSection.vue'
-import SectionFour from '../components/includes/SectionFour.vue'
+// import SectionFour from '../components/includes/SectionFour.vue'
 // import SectionOne from '../components/includes/SectionOne.vue'
 import SectionRow from '../components/includes/SectionRow.vue'
-import SectionScroll from '../components/includes/SectionScroll.vue'
-import SectionSubHero from '../components/includes/SectionSubHero.vue'
-import SectionThree from '../components/includes/SectionThree.vue'
-import SectionTwo from '../components/includes/SectionTwo.vue'
+// import SectionScroll from '../components/includes/SectionScroll.vue'
+// import SectionSubHero from '../components/includes/SectionSubHero.vue'
+// import SectionThree from '../components/includes/SectionThree.vue'
+// import SectionTwo from '../components/includes/SectionTwo.vue'
 import scrollTopMixin from '../mixins/scrollTopMixin'
 import SectionFeaturesOverview from '../components/includes/SectionFeaturesOverview.vue'
 export default {
-  components: { BannerSection, /*SectionOne,*/ SectionTwo, SectionThree, SectionFour, SectionScroll, SectionRow, SectionSubHero, SectionFeaturesOverview },
+  components: { BannerSection,/* SectionOne, SectionTwo SectionThree, SectionFour, SectionScroll, SectionSubHero,*/ SectionRow,  SectionFeaturesOverview },
   name: 'Home',
   computed: mapGetters(['getWindowHeight', 'getAppHostname']),
   mixins: [scrollTopMixin],
